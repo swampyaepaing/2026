39146d95824aecba4dfd4389c6892669a652f42d
Swam Pyae Paing <66564931+swampyaepaing@users.noreply.github.com>
2026-02-01 14:45:09 +0000

    Clean up duplicate image filenames in config.js
    Removed duplicate entries of image filenames.

--- config.js
+++ config.js
@@ -23,25 +23,8 @@ const CONFIG = {
         "2.jpg",
         "3.jpg",
         "4.jpg",
-        "5.jpg",
-
-        "1.jpg",
-        "2.jpg",
-        "3.jpg",
-        "4.jpg",
-        "5.jpg",
-
-        "1.jpg",
-        "2.jpg",
-        "3.jpg",
-        "4.jpg",
-        "5.jpg",
-
-        "1.jpg",
-        "2.jpg",
-        "3.jpg",
-        "4.jpg",
         "5.jpg"
+
 ],
     
 
