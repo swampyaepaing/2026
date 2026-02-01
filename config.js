4366c264a0b25d7340e28590f2d0530284ef2c59
Swam Pyae Paing <66564931+swampyaepaing@users.noreply.github.com>
2026-02-01 14:44:42 +0000

    Add additional image entries to config.js

--- config.js
+++ config.js
@@ -23,6 +23,24 @@ const CONFIG = {
         "2.jpg",
         "3.jpg",
         "4.jpg",
+        "5.jpg",
+
+        "1.jpg",
+        "2.jpg",
+        "3.jpg",
+        "4.jpg",
+        "5.jpg",
+
+        "1.jpg",
+        "2.jpg",
+        "3.jpg",
+        "4.jpg",
+        "5.jpg",
+
+        "1.jpg",
+        "2.jpg",
+        "3.jpg",
+        "4.jpg",
         "5.jpg"
 ],
     
